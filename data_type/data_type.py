ba133ee2bc24f782338e8feee169f2174a5a43a9
francosebastian <francosebastian22@gmail.com>
2020-05-04 00:29:56 -0400

    first commit

---
+++ data_type/data_type.py
@@ -0,0 +1,27 @@
+
+# Strings
+print("Hello world")
+print('Hello world')
+print('''Hello World''')
+print("""Hello World""")
+# Concat
+print("Bye "+"World")
+# Integer
+print(30)
+# Float
+print(30.5)
+# Boolean
+True
+False
+# List
+[10, 20, 30, 55]
+["Hello", "Bye", "Adios"]
+[10, "Hello", True, 10.1]
+# Tuples
+{10, 20, 30, 55}
+# Dictionaries
+{
+    "nombre": "Franco",
+ "apellido": "Sebastian",
+ "apodo": "Ethereal"
+ }
